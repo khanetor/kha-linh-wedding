7ddf054ed32be536e7b32539549e37b5ca2bf1a3
Kha Nguyen <kha.nguyen@reaktor.com>
2025-10-27 10:44:54 +0200

    use newer image viewer plugin

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,26 +1,23 @@
 // https://vitepress.dev/guide/custom-theme
 // import Layout from "./Layout.vue"
 // @ts-ignore
-import Countdown from "./components/Countdown.vue";
-import { h } from "vue";
-import type { Theme } from "vitepress";
-import DefaultTheme from "vitepress/theme";
-import "@catppuccin/vitepress/theme/macchiato/flamingo.css";
-import "viewerjs/dist/viewer.min.css";
-import "./style.css";
-import imageViewer from "vitepress-plugin-image-viewer";
-import { useRoute } from "vitepress";
+import Countdown from "./components/Countdown.vue"
+import { h } from "vue"
+import type { Theme } from "vitepress"
+import DefaultTheme from "vitepress/theme"
+// @ts-ignore
+import ImageViewerP from "@miletorix/vitepress-image-viewer"
+import "@miletorix/vitepress-image-viewer/style.css"
+import "@catppuccin/vitepress/theme/macchiato/flamingo.css"
+import "./style.css"
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    return h(DefaultTheme.Layout, null, {});
+    return h(DefaultTheme.Layout, null, {})
   },
-  enhanceApp({ app, router, siteData }) {
-    app.component("Countdown", Countdown);
+  enhanceApp({ app }) {
+    app.component("Countdown", Countdown)
+    ImageViewerP(app)
   },
-  setup() {
-    const route = useRoute();
-    imageViewer(route);
-  },
-} satisfies Theme;
+} satisfies Theme
